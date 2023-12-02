60f4c2f30fb93bc9d4f5f616b9444652538c51d0
AdamMawani <142749950+AdamMawani@users.noreply.github.com>
2023-12-01 22:51:25 -0500

    Add files via upload

---
+++ GARCHVolatilityPortfolio.py
@@ -0,0 +1,46 @@
+import yfinance as yf
+import numpy as np
+import pandas as pd
+from arch import arch_model
+import matplotlib.pyplot as plt
+
+# Define portfolio
+portfolio = {"AAPL": 0.1, "TSLA": 0.9}
+
+# Fetch historical data for each stock in the portfolio
+start_date = "2022-01-01"
+end_date = "2023-01-01"
+
+stock_data = {}
+for ticker, weight in portfolio.items():
+    data = yf.download(ticker, start=start_date, end=end_date)
+    stock_data[ticker] = {'data': data, 'weight': weight}
+
+# Calculate daily returns for each stock in the portfolio
+returns = pd.DataFrame()
+for ticker, stock_info in stock_data.items():
+    data = stock_info['data']
+    weight = stock_info['weight']
+    returns[ticker] = data['Adj Close'].pct_change().dropna() * 10 * weight
+
+# Calculate the portfolio returns as the sum of individual stock returns
+returns['Portfolio'] = returns.sum(axis=1)
+
+# Fit GARCH(1,1) model to the portfolio returns
+model = arch_model(returns['Portfolio'], vol='Garch', p=1, q=1)
+results = model.fit()
+
+# Display model summary
+print(results.summary())
+
+# Plot portfolio returns and conditional volatility
+fig, ax = plt.subplots(figsize=(10, 6))
+
+ax.plot(returns.index, returns['Portfolio'], label='Portfolio Returns')
+ax.plot(returns.index, results.conditional_volatility, label='Conditional Volatility (GARCH(1,1))', color='red')
+
+ax.set(title='Portfolio Returns and GARCH(1,1) Conditional Volatility',
+       xlabel='Date', ylabel='Returns / Volatility')
+ax.legend()
+
+plt.show()
