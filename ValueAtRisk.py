bea88e4b990dbc6254802c03ca7b399f41e4f0f6
AdamMawani <142749950+AdamMawani@users.noreply.github.com>
2024-02-05 15:31:53 -0500

    Update ValueAtRisk.py

--- ValueAtRisk.py
+++ ValueAtRisk.py
@@ -26,7 +26,7 @@ def calculate_stock_var(ticker, start_date, end_date):
 
 if __name__ == "__main__":
     # Example portfolio
-    portfolio = {"AAPL": 0.66, "ENB": 0.66, "QQQ": 0.66, "SPY": 0.66, "NVDA": 0.66, "MSFT": 0.66, "GOOG": 0.66, "MA": 0.66, "VZ": 0.66, "T": 0.66, "AMZN": 0.66, "C": 0.66, "RY": 0.66, "BNS": 0.66, "BAC": 0.66, "UBER": 0.66}
+    portfolio = {"ENB": 0.15, "QQQ": 0.15, "SPY": 0.05, "NVDA": 0.1, "MSFT": 0.07, "GOOG": 0.05, "VZ": 0.07, "T": 0.05, "AMZN": 0.07, "C": 0.03, "RY": 0.05, "BNS": 0.03, "BAC": 0.03, "MA": 0.05, "UBER": 0.05}
     stocks = ["AAPL", "ENB", "QQQ", "SPY", "NVDA", "MSFT", "GOOG", "MA", "VZ", "T", "AMZN", "C", "RY", "BNS", "BAC", "UBER"]
 
     # Historical data start and end dates
